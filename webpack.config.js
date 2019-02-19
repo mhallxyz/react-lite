247639f348fa3f81baf8b59373afd54455b0d2e5
Mathew Hall <mhall.js@gmail.com>
2019-02-19 10:18:55 +0000

    Re-design

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,47 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+import path from 'path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 module.exports = {
+  entry: path.join(__dirname,'src','index.js'),
+  output: {
+    path: path.join(__dirname,'build'),
+    filename: 'index.bundle.js'
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname,'src')
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        // this is so that we can compile any React,
+        // ES6 and above into normal ES5 syntax
+        test: /\.(js|jsx)$/,
+        // we do not want anything from node_modules to be compiled
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: ['babel-loader']
       },
       {
-        test: /\.html$/,
+        test: /\.(css|scss)$/,
         use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
         ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ['file-loader']
       }
     ]
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname,'src','index.html')
     })
   ]
 };
