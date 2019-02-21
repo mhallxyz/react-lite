fc874ab794d2bcd08f7bb0a6e524bade9b3fe259
Mathew Hall <mhall.js@gmail.com>
2019-02-21 10:47:11 +0000

    React Lite base styling added to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 // this line is new
 // we now have some nice styles on our react app
 import "index.scss";
-let HelloWorld = () => {
-  return <h1>Hello there World!</h1>
-}
-ReactDOM.render(
-  <HelloWorld/>,
-  document.getElementById("root")
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
